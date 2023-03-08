030b1151dfaa7b2790a10a8e863d123bcb317af3
Ordo Adrian <ordoadrian@ordos-mbp.home>
2023-03-08 17:22:12 +0000

    started styling work on home page. added memoji of myself and realigned everything but I dont think I like it, need to rework this

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,16 +2,20 @@ import React from "react";
 
 function Home() {
     return (
-        <div className="homeContainer">
-            <div className="titleText">
-                <h1>
-                    Hi! It's me Adrian, and this is my Portfolio. <br/>
-                    I enjoy building websites just like this one! <br/>
-                    I hope you have as much fun checking it out as I did building it!
-                </h1>
+        <div className="container text-center homeContainer">
+            <div className="row titleText">
+                <div className="col">
+                    <h1>
+                        Hi! It's me Adrian, and this is my Portfolio. <br/>
+                        I enjoy building websites just like this one! <br/>
+                        I hope you have as much fun checking it out as I did building it!
+                    </h1>
+                </div>
             </div>
-            <div className="image">
-                <img src="https://via.placeholder.com/500x500" alt="Adrian Kosnik looking at you" />
+            <div className="row">
+                <div className="col"></div>
+                <div className="col imageDiv"></div>
+                <div className="col"></div>
             </div>
         </div>
     );
