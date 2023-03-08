b310be684e81df188fa69ce258b9db2accac2bfa
Ordo Adrian <ordoadrian@ordos-mbp.home>
2023-03-08 16:37:04 +0000

    added skeleton for contact page. All pages now have the basic lay out and elements included. Now will move onto styling

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Contact() {
-    return <h1>This is the Contact page!</h1>
+    return (
+        <div className="masterContainer">
+            <div className="leftContainer">
+                <h1>Contact info:</h1>
+                <ul>
+                    <li>Github: <a href="https://github.com/Adrian-Kosnik">https://github.com/Adrian-Kosnik</a></li>
+                    <li>Linkedin: <a href="https://www.linkedin.com/in/adrian-kosnik-4a0945201/">https://www.linkedin.com/in/adrian-kosnik-4a0945201/</a></li>
+                    <li>Email: <a href="mailto:adriankosnik67@gmail.com">adriankosnik67@gmail.com</a></li>
+                </ul>
+            </div>
+            <div className="rightContainer">
+                <form>
+                <input placeholder="Your Name" />
+                <input placeholder="Your Message" />
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Contact;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,22 +1,19 @@
 import React from "react";
-import Wrapper from "../Wrapper/Wrapper";
 
 function Home() {
     return (
-        <>
-            <div className="homeContainer">
-                <div className="titleText">
-                    <h1>
-                        Hi! It's me Adrian, and this is my Portfolio. <br/>
-                        I enjoy building web apps and websites just like this one! <br/>
-                        I hope you have as much fun checking it out as I did building it!
-                    </h1>
-                </div>
-                <div className="image">
-                    <img src="https://via.placeholder.com/500x500" alt="Adrian Kosnik looking at you" />
-                </div>
+        <div className="homeContainer">
+            <div className="titleText">
+                <h1>
+                    Hi! It's me Adrian, and this is my Portfolio. <br/>
+                    I enjoy building websites just like this one! <br/>
+                    I hope you have as much fun checking it out as I did building it!
+                </h1>
             </div>
-        </>
+            <div className="image">
+                <img src="https://via.placeholder.com/500x500" alt="Adrian Kosnik looking at you" />
+            </div>
+        </div>
     );
 };
 
