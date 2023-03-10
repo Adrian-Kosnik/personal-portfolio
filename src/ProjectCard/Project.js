6126f536788dae88274c3fd1ac43227ea881e202
Ordo Adrian <ordoadrian@ordos-mbp.home>
2023-03-10 13:35:58 +0000

    fixed up project cards, added modal. still need to add all content to modal

--- src/ProjectCard/Project.js
+++ src/ProjectCard/Project.js
@@ -5,9 +5,32 @@ function ProjectCard(props) {
         <div className="card" style={{width: "18rem"}}>
             <img src={props.image} className="card-img-top" alt={props.title} />
             <div className="card-body">
-                <h5 className="card-title">{props.title}</h5>
-                <p className="card-text">{props.description}</p>
-                <a href={props.liveGithub} target="blank" className="btn btn-primary">Check it!</a>
+                {/* <!-- Button trigger modal --> */}
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    {props.title}
+                </button>
+
+                {/* <!-- Modal --> */}
+                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        noononnono
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Understood</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                {/* <a><h5 className="card-title">{props.title}</h5></a> */}
+                {/* <p className="card-text">{props.description}</p> */}
+                {/* <a href={props.liveGithub} target="blank" className="btn btn-primary">Check it!</a> */}
             </div>
         </div>
     );
