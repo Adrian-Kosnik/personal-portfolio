76c95697e635c4dc47031ad2886cb482db6ae0c4
Ordo Adrian <ordoadrian@ordos-mbp.home>
2023-03-11 22:38:20 +0000

    I have removed previous navbar and inserted bootstrap navbar instead

--- src/App.js
+++ src/App.js
@@ -4,21 +4,12 @@ import ProjectGallery from './Pages/ProjectGallery';
 import Contact from './Pages/Contact';
 import Navbar from './Header/Navbar';
 import { Route, Routes } from "react-router-dom";
-import Pointer from './Header/Pointer';
 
 function App() {
 
   return (
     <div className='appContainer'>
-      <div className='container col-3'>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Pointer />} />
-          <Route path="/personal-portfolio" element={<Pointer />} />
-          <Route path="/ProjectGallery" element={<Pointer />} />
-          <Route path="/Contact" element={<Pointer />} />
-        </Routes>
-      </div>
+      <Navbar />
       <div className='container'>
         <Routes>
           <Route path="/" element={<Home />} />
--- src/Header/Navbar.js
+++ src/Header/Navbar.js
@@ -1,20 +1,49 @@
 import React from "react";
-import { Link, useMatch, useResolvedPath } from "react-router-dom";
+// import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+// function Navbar() {
+//     return (
+//         <div className="outerNavContainer">
+//             <CustomLink to="/"><img src={require("../assets/icons/home.png")} className="home" /></CustomLink>
+//             <CustomLink to="/ProjectGallery"><img src={require("../assets/icons/briefcase.png")} className="projects" /></CustomLink>
+//             <CustomLink to="/Contact"><img src={require("../assets/icons/envelope.png")} className="contact" /></CustomLink>
+//         </div>
+//     );
+// };
 
 function Navbar() {
     return (
-        <div className="outerNavContainer">
-            <CustomLink to="/"><img src={require("../assets/icons/home.png")} className="home" /></CustomLink>
-            <CustomLink to="/ProjectGallery"><img src={require("../assets/icons/briefcase.png")} className="projects" /></CustomLink>
-            <CustomLink to="/Contact"><img src={require("../assets/icons/envelope.png")} className="contact" /></CustomLink>
-        </div>
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 };
 
-function CustomLink({to, children, ...props}) {
-    const resolvedPath = useResolvedPath(to);
-    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
-    return <Link className={isActive ? "active" : ""} to={to} {...props}>{children}</Link>
-};
+// function CustomLink({to, children, ...props}) {
+//     const resolvedPath = useResolvedPath(to);
+//     const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+//     return <Link className={isActive ? "active" : ""} to={to} {...props}>{children}</Link>
+// };
 
 export default Navbar;
