c3730774a6ad0c0c1dceb4e26a2ba19e072c617c
Ordo Adrian <ordoadrian@ordos-mbp.home>
2023-03-12 12:28:03 +0000

    started on contact page, reworking the design, adding white background for the rest of the form to be placed on

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,35 +1,37 @@
 import React from "react";
-import Background from "../Wrapper/Background";
 
 function Contact() {
     return (
-        <Background>
-            <div className="masterContainer">
-                <div className="details-container">
-                    <h1>Contact Me</h1>
-                    <ul>
-                        <li>Github: <a href="https://github.com/Adrian-Kosnik">https://github.com/Adrian-Kosnik</a></li>
-                        <li>Linkedin: <a href="https://www.linkedin.com/in/adrian-kosnik-4a0945201/">https://www.linkedin.com/in/adrian-kosnik-4a0945201/</a></li>
-                        <li>Email: <a href="mailto:adriankosnik67@gmail.com">adriankosnik67@gmail.com</a></li>
-                        <li>Resume: <a href="/">My CV PDF</a></li>
-                    </ul>
-                </div>
-                <div className="input-container">
-                    <form>
-                        <div className="name-email-container">
-                            <input className="name-input col-4" placeholder="Name" />
-                            <input className="email-input col-4" placeholder="Email" />
-                        </div>
-                        <div className="subject-container">
-                            <input className="subject-input col-8" placeholder="Subject" />
-                        </div>
-                        <div className="message-container">
-                            <textarea className="message-textarea col-8" placeholder="Message" />
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </Background>
+        // <div className="masterContainer">
+        //     <div className="details-container">
+        //         <h1>Contact Me</h1>
+        //         <ul>
+        //             <li>Github: <a href="https://github.com/Adrian-Kosnik">https://github.com/Adrian-Kosnik</a></li>
+        //             <li>Linkedin: <a href="https://www.linkedin.com/in/adrian-kosnik-4a0945201/">https://www.linkedin.com/in/adrian-kosnik-4a0945201/</a></li>
+        //             <li>Email: <a href="mailto:adriankosnik67@gmail.com">adriankosnik67@gmail.com</a></li>
+        //             <li>Resume: <a href="/">My CV PDF</a></li>
+        //         </ul>
+        //     </div>
+        //     <div className="input-container">
+        //         <form>
+        //             <div className="name-email-container">
+        //                 <input className="name-input col-4" placeholder="Name" />
+        //                 <input className="email-input col-4" placeholder="Email" />
+        //             </div>
+        //             <div className="subject-container">
+        //                 <input className="subject-input col-8" placeholder="Subject" />
+        //             </div>
+        //             <div className="message-container">
+        //                 <textarea className="message-textarea col-8" placeholder="Message" />
+        //             </div>
+        //         </form>
+        //     </div>
+        // </div>
+        <div className="outer-contact-container">
+            <div className="contact-container"></div>
+        </div>
+
+
     );
 };
 
