8844a7521c058ebc8daf91faf98795427aaf59c0
Ordo Adrian <ordoadrian@ordos-mbp.home>
2023-03-12 20:24:47 +0000

    contacts page layout is now finished, still needs finishing touches when it comes to small details but its pretty much done now

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -2,46 +2,11 @@ import React from "react";
 
 function Contact() {
     return (
-        // <div className="masterContainer">
-        //     <div className="details-container">
-        //         <h1>Contact Me</h1>
-        //         <ul>
-        //             <li>Github: <a href="https://github.com/Adrian-Kosnik">https://github.com/Adrian-Kosnik</a></li>
-        //             <li>Linkedin: <a href="https://www.linkedin.com/in/adrian-kosnik-4a0945201/">https://www.linkedin.com/in/adrian-kosnik-4a0945201/</a></li>
-        //             <li>Email: <a href="mailto:adriankosnik67@gmail.com">adriankosnik67@gmail.com</a></li>
-        //             <li>Resume: <a href="/">My CV PDF</a></li>
-        //         </ul>
-        //     </div>
-            // <div className="input-container">
-            //     <form>
-            //         <div className="name-email-container">
-            //             <input className="name-input col-4" placeholder="Name" />
-            //             <input className="email-input col-4" placeholder="Email" />
-            //         </div>
-            //         <div className="subject-container">
-            //             <input className="subject-input col-8" placeholder="Subject" />
-            //         </div>
-            //         <div className="message-container">
-            //             <textarea className="message-textarea col-8" placeholder="Message" />
-            //         </div>
-            //     </form>
-            // </div>
-        // </div>
         <div className="container outer-contact-container col-12">
             <div className="container inner-outer-contact-container col-12">
                 <div className="container title-contact-container col-6">
                     <h1>Get in touch!</h1>
                     <div className="contact-lists-container">
-                        {/* <ul className="contact-list-titles">
-                            <li>Email</li>
-                            <li>Linkedin</li>
-                            <li>Github</li>
-                        </ul>
-                        <ul className="contact-list-values">
-                            <li><a href="mailto:adriankosnik67@gmail.com">adriankosnik67@gmail.com</a></li>
-                            <li><a href="https://www.linkedin.com/in/adrian-kosnik-4a0945201/">linkedin.com/in/adrian-kosnik-4a0945201</a></li>
-                            <li><a href="https://github.com/Adrian-Kosnik">github.com/Adrian-Kosnik</a></li>
-                        </ul> */}
                         <a className="contact-button" href="mailto:adriankosnik67@gmail.com">Email</a>
                         <a className="contact-button" href="mailto:adriankosnik67@gmail.com">Linkedin</a>
                         <a className="contact-button" href="mailto:adriankosnik67@gmail.com">Github</a>
@@ -53,18 +18,16 @@ function Contact() {
                                 <h1>Any questions?</h1>
                                 <h2>If so drop me a message, I'd love to hear form you!</h2>
                             </div>
-                            <form className="container ">
+                            <form className="container">
                                 <div className="name-email-container">
                                     <input className="name-input" placeholder="Name" />
-                                </div>
-                                <div className="name-email-container">
                                     <input className="email-input" placeholder="Email" />
                                 </div>
                                 <div className="subject-container">
                                     <input className="subject-input" placeholder="Subject" />
                                 </div>
                                 <div className="message-container">
-                                    <input className="message-textarea" placeholder="Message" />
+                                    <textarea className="message-textarea" rows="10" placeholder="Message" />
                                 </div>
                             </form>
                     </div>
