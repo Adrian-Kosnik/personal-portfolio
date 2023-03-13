ab445f606965299f8daf146d065513bc682f9dbc
Ordo Adrian <ordoadrian@ordos-mbp.home>
2023-03-13 11:53:40 +0000

    added nowrap to home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,8 @@
 import React from "react";
-import Background from "../Wrapper/Background";
 
 function Home() {
     return (
-        <div className="booty">
+        <div className="outer-outerHomeContainer">
             <div className="outerHomeContainer">
                 <div className="titleContainer">
                     <h1>Adrian Kosnik</h1>
